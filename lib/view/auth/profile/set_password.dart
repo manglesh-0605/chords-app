0f83dd59013462ca9a4b9fd0aad46fe4af33a6f6
Manglesh <manglesh0605@gmail.com>
2024-11-24 15:40:18 +0530

    updated the set password screen

--- lib/view/auth/profile/set_password.dart
+++ lib/view/auth/profile/set_password.dart
@@ -15,115 +15,141 @@ class SetPassword extends StatelessWidget {
     TextEditingController confPassword = TextEditingController();
     ValueNotifier obscureOldPassword = ValueNotifier<bool>(true);
     ValueNotifier obscureNewPassword = ValueNotifier<bool>(true);
+
     return Scaffold(
       appBar: AppBar(
         leading: backIcon(context),
         title: const Text("Set new password"),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.w),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              spaceHeight(20.h),
-              Text(
-                "Old Password",
-                style: textStyle(weight: FontWeight.w400),
-              ),
-              spaceHeight(),
-              ValueListenableBuilder(
-                builder: (BuildContext context, value, Widget? child) {
-                  return AppTextField(
-                    controller: oldPassword,
-                    keyType: TextInputType.text,
-                    obscureText: obscureOldPassword.value,
-                    hint: "Old Password",
-                    suffixIcon: IconButton(
-                      splashColor: Colors.transparent,
-                      icon: Icon(
-                        obscureOldPassword.value
-                            ? Icons.visibility_off_outlined
-                            : Icons.visibility_outlined,
-                        color: AppColors.grey,
-                        size: 24.r,
-                      ),
-                      onPressed: () {
-                        obscureOldPassword.value = !obscureOldPassword.value;
-                      },
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.w),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  spaceHeight(20.h),
+                  Text(
+                    "Old Password",
+                    style: textStyle(weight: FontWeight.w400),
+                  ),
+                  spaceHeight(),
+                  ValueListenableBuilder(
+                    builder: (BuildContext context, value, Widget? child) {
+                      return AppTextField(
+                        controller: oldPassword,
+                        keyType: TextInputType.text,
+                        obscureText: obscureOldPassword.value,
+                        hint: "Old Password",
+                        suffixIcon: IconButton(
+                          splashColor: Colors.transparent,
+                          icon: Icon(
+                            obscureOldPassword.value
+                                ? Icons.visibility_off_outlined
+                                : Icons.visibility_outlined,
+                            color: AppColors.grey,
+                            size: 24.r,
+                          ),
+                          onPressed: () {
+                            obscureOldPassword.value =
+                                !obscureOldPassword.value;
+                          },
+                        ),
+                      );
+                    },
+                    valueListenable: obscureOldPassword,
+                  ),
+                  spaceHeight(20.h),
+                  Text(
+                    "Set New Password",
+                    style: textStyle(weight: FontWeight.w400),
+                  ),
+                  spaceHeight(),
+                  ValueListenableBuilder(
+                    builder: (BuildContext context, value, Widget? child) {
+                      return AppTextField(
+                        controller: newPassword,
+                        keyType: TextInputType.text,
+                        obscureText: obscureNewPassword.value,
+                        hint: "New Password",
+                        suffixIcon: IconButton(
+                          splashColor: Colors.transparent,
+                          icon: Icon(
+                            obscureNewPassword.value
+                                ? Icons.visibility_off_outlined
+                                : Icons.visibility_outlined,
+                            color: AppColors.grey,
+                            size: 24.r,
+                          ),
+                          onPressed: () {
+                            obscureNewPassword.value =
+                                !obscureNewPassword.value;
+                          },
+                        ),
+                      );
+                    },
+                    valueListenable: obscureNewPassword,
+                  ),
+                  spaceHeight(),
+                  ValueListenableBuilder(
+                    builder: (BuildContext context, value, Widget? child) {
+                      return AppTextField(
+                        controller: confPassword,
+                        keyType: TextInputType.text,
+                        obscureText: obscureNewPassword.value,
+                        hint: "Re-Password",
+                        suffixIcon: IconButton(
+                          splashColor: Colors.transparent,
+                          icon: Icon(
+                            obscureNewPassword.value
+                                ? Icons.visibility_off_outlined
+                                : Icons.visibility_outlined,
+                            color: AppColors.grey,
+                            size: 24.r,
+                          ),
+                          onPressed: () {
+                            obscureNewPassword.value =
+                                !obscureNewPassword.value;
+                          },
+                        ),
+                      );
+                    },
+                    valueListenable: obscureNewPassword,
+                  ),
+                  spaceHeight(60.h),
+                  Container(
+                    height: 40.h,
+                    color: AppColors.blackGrey,
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Update Password",
+                      style: textStyle(weight: FontWeight.w400),
                     ),
-                  );
-                },
-                valueListenable: obscureOldPassword,
+                  ),
+                ],
               ),
-              spaceHeight(20.h),
-              Text(
-                "Set New Password",
-                style: textStyle(weight: FontWeight.w400),
-              ),
-              spaceHeight(),
-              ValueListenableBuilder(
-                builder: (BuildContext context, value, Widget? child) {
-                  return AppTextField(
-                    controller: newPassword,
-                    keyType: TextInputType.text,
-                    obscureText: obscureNewPassword.value,
-                    hint: "New Password",
-                    suffixIcon: IconButton(
-                      splashColor: Colors.transparent,
-                      icon: Icon(
-                        obscureNewPassword.value
-                            ? Icons.visibility_off_outlined
-                            : Icons.visibility_outlined,
-                        color: AppColors.grey,
-                        size: 24.r,
-                      ),
-                      onPressed: () {
-                        obscureNewPassword.value = !obscureNewPassword.value;
-                      },
-                    ),
-                  );
-                },
-                valueListenable: obscureNewPassword,
-              ),
-              spaceHeight(),
-              ValueListenableBuilder(
-                builder: (BuildContext context, value, Widget? child) {
-                  return AppTextField(
-                    controller: confPassword,
-                    keyType: TextInputType.text,
-                    obscureText: obscureNewPassword.value,
-                    hint: "Re-Password",
-                    suffixIcon: IconButton(
-                      splashColor: Colors.transparent,
-                      icon: Icon(
-                        obscureNewPassword.value
-                            ? Icons.visibility_off_outlined
-                            : Icons.visibility_outlined,
-                        color: AppColors.grey,
-                        size: 24.r,
-                      ),
-                      onPressed: () {
-                        obscureNewPassword.value = !obscureNewPassword.value;
-                      },
-                    ),
-                  );
-                },
-                valueListenable: obscureNewPassword,
-              ),
-              spaceHeight(60.h),
-              Container(
-                height: 40.h,
-                color: AppColors.blackGrey,
-                alignment: Alignment.center,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 44.h,
+              width: double.infinity,
+              color: Colors.blue, // Replace with your desired banner color
+              child: Center(
                 child: Text(
-                  "Update Password",
-                  style: textStyle(weight: FontWeight.w400),
+                  'App Banner',
+                  style: textStyle(
+                    fontSize: 18.sp,
+                    weight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
