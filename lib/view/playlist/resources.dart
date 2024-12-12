335a05a0a4b5c7169dd61b8f503c61c46b605454
Manglesh <manglesh0605@gmail.com>
2024-12-12 20:15:49 +0530

    Changes in UI

--- lib/view/playlist/resources.dart
+++ lib/view/playlist/resources.dart
@@ -11,20 +11,46 @@ class ResourcesScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Resources"),
       ),
-      body: GridView.builder(
-        itemCount: 10,
-        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: screenWidth * 0.6,
-            crossAxisSpacing: 16.h,
-            mainAxisSpacing: 16.h,
-            mainAxisExtent: 86.h),
-        itemBuilder: (context, index) {
-          return ClipRRect(
-            borderRadius: BorderRadius.circular(6.r),
-            child: Image.asset("assets/images/blog.png"),
-          );
-        },
+      body: Stack(
+        children: [
+          // Main content
+
+          GridView.builder(
+            itemCount: 10,
+            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: screenWidth * 0.6,
+                crossAxisSpacing: 16.h,
+                mainAxisSpacing: 16.h,
+                mainAxisExtent: 86.h),
+            itemBuilder: (context, index) {
+              return ClipRRect(
+                borderRadius: BorderRadius.circular(6.r),
+                child: Image.asset("assets/images/blog.png"),
+              );
+            },
+          ),
+
+          // Banner at the bottom
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 44.h,
+              width: double.infinity,
+              color: Colors.blue,
+              child: Center(
+                child: Text(
+                  "App Banner",
+                  style: textStyle(
+                    color: Colors.white,
+                    fontSize: 20.sp,
+                    weight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
